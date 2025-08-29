9710c9299400a737d2ead88ab8ea63a9626a6276
gimhyeong-gon <gudrhs8304@gmail.com>
2025-08-29 23:15:22 +0900

    250829 - 순매출, 승인매출, TopN 구현

---
+++ src/main/java/com/gudrhs8304/ticketory/config/jpa/BitBooleanConverter.java
@@ -0,0 +1,20 @@
+package com.gudrhs8304.ticketory.config.jpa;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = false) // 필요한 필드에만 @Convert로 적용
+public class BitBooleanConverter implements AttributeConverter<Boolean, Byte> {
+
+    @Override
+    public Byte convertToDatabaseColumn(Boolean attribute) {
+        if (attribute == null) return null;  // 컬럼이 NULL 허용이면 그대로
+        return (attribute ? (byte) 1 : (byte) 0);
+    }
+
+    @Override
+    public Boolean convertToEntityAttribute(Byte dbData) {
+        if (dbData == null) return null;     // 컬럼이 NULL 허용이면 그대로
+        return dbData != 0;
+    }
+}
--- src/main/java/com/gudrhs8304/ticketory/domain/Movie.java
+++ src/main/java/com/gudrhs8304/ticketory/domain/Movie.java
@@ -45,9 +45,10 @@ public class Movie extends BaseTimeEntity {
     @Column(name = "release_date")
     private LocalDate releaseDate;
 
-    /** 현재 상영 여부 */
-    @Column(nullable = false)
+
+    @Column(name = "status", nullable = false, columnDefinition = "TINYINT(1)")
     private Boolean status;
+
 
     @Lob
     private String actors; // 필요 시 별도 테이블로 분리
